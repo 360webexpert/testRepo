e6bad9d3ddf4443f47c4e453e41583eafaf69023
Web Expert <36933789+360webexpert@users.noreply.github.com>
2024-07-10 13:22:31 +0530

    Update index.js

--- pages/history/index.js
+++ pages/history/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import DefaultLayout from '../../components/Layouts/DefaultLayout'
+import DefaultLayout from '../../components/Layouts/defaultLayout'
 
 const index = () => {
   
